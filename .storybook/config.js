0217f4838113d5e06221f8869ea2149f089a5e8d
John Livingston <jmlivingston@gmail.com>
2019-10-19 15:26:32 -0700

    Minimal setup

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+import { addParameters, configure } from '@storybook/react'
+import { themes } from '@storybook/theming'
+import 'bootswatch/dist/darkly/bootstrap.min.css'
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+})
+
+configure(require.context('../src', true, /\.stories\.js$/), module)
---
+++ src/App.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+
+export default {
+  title: 'App'
+}
+
+export const app1 = () => <App />
+
+export const app2 = () => <App />
